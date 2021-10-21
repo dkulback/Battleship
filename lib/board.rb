77c579cc23af6c6cd5c9f7f511f9abd6b1cc0908
Eldridge Turambi <eturambi95@gmail.com>
2021-10-21 11:13:19 -0600

    Fixed and refactor code and test for #valid_cordinate? method

--- lib/board.rb
+++ lib/board.rb
@@ -20,17 +20,6 @@ class Board
     "D4" => Cell.new('D4')}
   end
 
-  def create_cells
-    cell_letters = ['A', 'B', 'C', 'D']
-    cell_numbers = ['1', '2', '3', '4']
-
-    cell_hash = cell_letters.each do |letter|
-      cell_numbers.each do |number|
-        @cells["#{letter}#{number}"] = Cell.new("#{letter}#{number}")
-      end
-    end
-    cell_hash
-  end
 
   def valid_coordinate?(coordinate_check)
     ## The '.any?' method returns true if at least 1 of the collection elements is equal to whatever you put in as the argument
