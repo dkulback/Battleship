4381814624e65d7867f77c62b0903f6de59b23af
Eldridge Turambi <eturambi95@gmail.com>
2021-10-21 22:53:57 -0600

    Uncomment tests in #vertical_check spec to make them pass

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -53,8 +53,8 @@ RSpec.describe Board do
     it 'checks if coordinates are consecutive' do
       expect(@board.valid_placement?(@cruiser, ["A1", "A2", "A4"])).to be(false)
       expect(@board.valid_placement?(@submarine, ["A1", "B2"])).to be(true)
-      # expect(@board.valid_placement?(@cruiser, ["A3", "A2", "A1"])).to be(false)
-      # expect(@board.valid_placement?(@submarine, ["C1", "B1"])).to be(false)
+      expect(@board.valid_placement?(@cruiser, ["A3", "A2", "A1"])).to be(false)
+      expect(@board.valid_placement?(@submarine, ["C1", "B1"])).to be(false)
     end
   end
 
