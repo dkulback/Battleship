09f3c958ad3821a709bb772177f62bf09f23c76e
Darren Kulback <dkulback@gmail.com>
2021-10-24 17:44:48 -0600

    Testing ship generator

---
+++ battleship_runner.rb
@@ -0,0 +1,7 @@
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+require './lib/computer'
+
+computer = Computer.new
+computer.welcome_message
--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -11,15 +11,12 @@ RSpec.describe "it is a computer board" do
     expect(@computer).to be_a(Computer)
   end
 
-  xit 'places ships at random' do
+  it 'places ships at random' do
     @computer = Computer.new
     @cruiser = Ship.new("Cruiser", 3)
-    cell_1 = @board.cells["A1"]
-    cell_2 = @board.cells["A2"]
-    cell_3 = @board.cells["A3"]
-    cell_4 = @board.cells["A4"]
-    @computer.random_ship(@cruiser)
-    expect(cell_1).to eq(cruiser)
+
+    @computer.random_ships
+    expect(@computer.include?(cruiser)).to eq(cruiser)
   end
 
 end
