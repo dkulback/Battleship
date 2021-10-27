1dd75c200651a46c102ddcc39dd109a1f5a925e9
Darren Kulback <dkulback@gmail.com>
2021-10-27 14:48:58 -0600

    Close out extra spaces

--- lib/game.rb
+++ lib/game.rb
@@ -126,13 +126,22 @@ class Game
   def valid_placement_creator(ship)
     valid_sub_placements = [['A1', 'A2'], ['A2', 'A3'], ['A3','A4'], ['B1', 'B2'], ['B2', 'B3'], ['B3','B4'], ['C1', 'C2'], ['C2', 'C3'], ['C3','C4'], ['D1', 'D2'], ['D2', 'D3'], ['D3','D4'], ['A1', 'B1'], ['B1', 'C1'], ['C1','D1'], ['A2', 'B2'], ['B2', 'C2'], ['C2','D2'], ['A3', 'B3'], ['B3', 'C3'], ['C3','D3'], ['A4', 'B4'], ['B4', 'C4'], ['C4','D4']]
 
-    valid_cruiser_placements = battleship_array= [["A2", "A3", "A4"], [ "B2", "B3","B4"], [ "C2", "C3","C4"], [ "D2", "D3","D4"], ["A1", "B1", "C1"], ["A2", "B2", "C2"], ["A3", "B3", "C3"], ["A4", "B4", "C4"], ["A2", "A3", "A4"], ["B2", "B3", "B4"], [ "C2", "C3", "C4"], [ "D2", "D3", "D4"], [ "B1", "C1", "D1"], [ "B2", "C2", "D2"], [ "B3", "C3", "D3"], ["B4", "C4", "D4"]]
+    valid_cruiser_placements = [["A2", "A3", "A4"], [ "B2", "B3","B4"], [ "C2", "C3","C4"], [ "D2", "D3","D4"], ["A1", "B1", "C1"], ["A2", "B2", "C2"], ["A3", "B3", "C3"], ["A4", "B4", "C4"], ["A2", "A3", "A4"], ["B2", "B3", "B4"], [ "C2", "C3", "C4"], [ "D2", "D3", "D4"], [ "B1", "C1", "D1"], [ "B2", "C2", "D2"], [ "B3", "C3", "D3"], ["B4", "C4", "D4"]]
 
     if ship.length == 2
       valid_sub_placements.sample(1).flatten
     elsif ship.length == 3
       valid_cruiser_placements.sample(1).flatten
     end
+    ## We know this works it just takes forever to test 
+    # valid_random_position = @computer_board.cells.keys.sample(ship.length)
+    #
+    # until @computer_board.valid_placement?(ship, valid_random_position) == true
+    #  valid_random_position = @computer_board.cells.keys.sample(ship.length)
+    #
+    #  puts valid_random_position
+    # end
+    # valid_random_position
   end
 
   def comp_ship_generator
