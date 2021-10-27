7e9bc880ad74b7a28f4342c382e5a27b4ab637a5
Eldridge Turambi <eturambi95@gmail.com>
2021-10-27 11:49:02 -0600

    Final Refactor for iteration 3

--- lib/game.rb
+++ lib/game.rb
@@ -19,20 +19,25 @@ class Game
   end
 
   def welcome_message
-    puts "Welcome to Battleship"
+    puts "Welcome to"
+    puts "   ___       __  __  __        __   _
+  / _ )___ _/ /_/ /_/ /__ ___ / /  (_)__
+ / _  / _ `/ __/ __/ / -_|_-</ _ \/ / _ \
+/____/\_,_/\__/\__/_/\__/___/_//_/_/ .__/
+                                  /_/    "
     puts "Enter p to play. Enter q to quit"
     response = gets.chomp.downcase
       if response == "p"
         start_game
       elsif response == "q"
         puts "Game ending.."
-        exit 
+        exit
       end
   end
 
   def render_boards
     puts "-----------COMPUTER BOARD-------------"
-    puts @computer_board.render(true)
+    puts @computer_board.render
     puts "-----------PLAYER BOARD---------------"
     puts @player_board.render(true)
     puts "=" * 60
@@ -40,13 +45,13 @@ class Game
 
 
   def start_game
-    puts "I have laid out my ships on the grid.\nYou now need to lay out your two ships.\nThe Cruiser is three units long and the Submarine is two units long.\n #{@player_board.render}\nEnter the squares for the Cruiser (3 spaces):\n>"
+    puts "I have laid out my ships on the grid.\nYou now need to lay out your two ships.\nThe Cruiser is three units long and the Submarine is two units long.\n#{@player_board.render}\nEnter the squares for the Cruiser (3 spaces). \nExample: a1 b1 c1"
+    user_cruiser = gets.chomp.upcase.split.sort
 
-    user_cruiser = gets.chomp.upcase.split
     @player_board.valid_placement?(@cruiser, user_cruiser)
     until @player_board.valid_placement?(@cruiser, user_cruiser) == true
       puts "Invalid placement. Please enter valid placement for your cruiser(3 coordinates) \n>"
-      user_cruiser = gets.chomp.upcase.split
+      user_cruiser = gets.chomp.upcase.split.sort
       @player_board.valid_placement?(@cruiser, user_cruiser)
     end
     @player_board.place(@cruiser, user_cruiser)
@@ -54,11 +59,11 @@ class Game
 
     puts "Now place your submarine(2 coordinates)"
 
-    user_submarine = gets.chomp.upcase.split
+    user_submarine = gets.chomp.upcase.split.sort
     @player_board.valid_placement?(@submarine, user_submarine)
     until @player_board.valid_placement?(@submarine, user_submarine) == true
-      puts "Invalid placement. Please enter valid placement for your cruiser(2 coordinates) \n>"
-      user_submarine = gets.chomp.upcase.split
+      puts "Invalid placement. Please enter valid placement for your cruiser(2 coordinates)"
+      user_submarine = gets.chomp.upcase.split.sort
       @player_board.valid_placement?(@submarine, user_submarine)
     end
     @player_board.place(@submarine, user_submarine)
@@ -67,40 +72,75 @@ class Game
     # puts "Great position.\n #{@player_board.render(true)}"
   end
 
+  def user_turn_fire
+    puts "Bad Person: TRY TO SINK ALL OF MY SHIPS IF YOU CAN MUAHAHAHA!"
+    puts "Pick the coordinate of the bad guy that you want to fire at!"
+
+    user_fire = gets.chomp.upcase
+    until @computer_board.valid_coordinate?(user_fire) == true && @computer_board.cells[user_fire].fired_upon? == false
+      if @computer_board.valid_coordinate?(user_fire) == false
+      puts "Please enter a valid coordinate"
+      elsif @computer_board.cells[user_fire].fired_upon? == true
+        puts "You have already fired at this coordinate. Please pick another one."
+      end
+      user_fire = gets.chomp.upcase
+    end
+    @computer_board.fire(user_fire)
+    # if @computer_board.cells[user_fire].fired_upon? == false
+    #   puts "!!!!!!You already fired at this coordinate, please pick another one!!!!!!!!!!!"
+    # elsif @computer_board.cells[user_fire].fired_upon? == false
+    #   @computer_board.fire(user_fire)
+    # end
+
+    if @computer_board.cells[user_fire].render == "M"
+      puts "Your shot at #{user_fire} was a miss."
+    elsif @computer_board.cells[user_fire].render == "H"
+      puts "Your shot at #{user_fire} was a hit!."
+    elsif @computer_board.cells[user_fire].render == "X"
+      puts "You sunk my battleship!"
+    end
+  end
+
+  def computer_fire
+    puts "Computer will fire at you."
+    comp_fire = @player_board.cells.keys.sample
+    until @player_board.cells[comp_fire].fired_upon? == false
+      comp_fire = @player_board.cells.keys.sample
+    end
+    @player_board.fire(comp_fire)
+
+    if @player_board.cells[comp_fire].render == "M"
+      puts "My shot at #{comp_fire} was a miss."
+    elsif @player_board.cells[comp_fire].render == "H"
+      puts "My shot at #{comp_fire} was a hit!."
+    elsif @player_board.cells[comp_fire].render == "X"
+      puts "I sunk your battleship!"
+    end
+  end
+
   def turns
     until (@cruiser.sunk? && @submarine.sunk?) || (@cruiser_2.sunk? && @submarine_2.sunk?)
-      puts "Your turn to FIRE!"
 
-      user_fire = gets.chomp.upcase
-      until @computer_board.valid_coordinate?(user_fire) == true
-        puts "Please enter a valid coordinate"
-        user_fire = gets.chomp.upcase
-      end
-      @computer_board.fire(user_fire)
-
-      puts "Computer will fire at you."
-      comp_fire = @player_board.cells.keys.sample
-      until @player_board.cells[comp_fire].fired_upon? == false
-        comp_fire = @player_board.cells.keys.sample
-      end
-      @player_board.fire(comp_fire)
-
-      if @computer_board.cells[user_fire].render == "M"
-        puts "Your shot at #{user_fire} was a miss."
-      elsif @computer_board.cells[user_fire].render == "H"
-        puts "Your shot at #{user_fire} was a hit!."
-      elsif @computer_board.cells[user_fire].render == "X"
-        puts "You sunk my battleship!"
-      end
-
-      if @player_board.cells[comp_fire].render == "M"
-        puts "My shot at #{comp_fire} was a miss."
-      elsif @player_board.cells[comp_fire].render == "H"
-        puts "My shot at #{comp_fire} was a hit!."
-      elsif @player_board.cells[comp_fire].render == "X"
-        puts "I sunk your battleship!"
-      end
+      user_turn_fire
+      computer_fire
       render_boards
+
+      # if @computer_board.cells[user_fire].render == "M"
+      #   puts "Your shot at #{user_fire} was a miss."
+      # elsif @computer_board.cells[user_fire].render == "H"
+      #   puts "Your shot at #{user_fire} was a hit!."
+      # elsif @computer_board.cells[user_fire].render == "X"
+      #   puts "You sunk my battleship!"
+      # end
+
+      # if @player_board.cells[comp_fire].render == "M"
+      #   puts "My shot at #{comp_fire} was a miss."
+      # elsif @player_board.cells[comp_fire].render == "H"
+      #   puts "My shot at #{comp_fire} was a hit!."
+      # elsif @player_board.cells[comp_fire].render == "X"
+      #   puts "I sunk your battleship!"
+      # end
+      # render_boards
     end
   end
 
